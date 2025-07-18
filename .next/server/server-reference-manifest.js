ae64a8f93d13b8981f704c4c3435bbc880ab12ab
Alexander T <alexander@awideweb.com>
2025-07-18 19:38:22 -0400

    Vercel Fix 1.1

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zEtt/vx2CvXwKgpcurIEl9BphV4aBM3SC8Tv2Yv5F5M=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
